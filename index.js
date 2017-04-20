a814c2eba22b7e914b2a601c2a92fa84777c59b4
bobby <jeremy.felicite@gmail.com>
2017-04-20 11:58:44 +0200

    add call api search for getting data on hashtag

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const app = express();
 
 require('./settings')(app);
 
+let twitter = new Twit(app.settings.api.twitter);
+
 console.log(`server listening on port ${app.settings.port}`);
 app.listen(app.settings.port);
+
+twitter.get('search/tweets', { q: '@twinpeaks since:2011-07-11', count: 100 }, function(err, data, response) {
+  console.log(data)
+});
